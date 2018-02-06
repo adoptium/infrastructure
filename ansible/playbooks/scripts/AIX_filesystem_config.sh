96ac528675b3e4ff7d1a178b4874b480d97a91e5
Joe deKoning <joe_dekoning@ca.ibm.com>
2018-02-06 14:32:45 -0500

    Update AIX scripts (#118)
    * filesystem setup for jenkins user in /home
    * add checks prior to performing task to speed update
    * replace most all rpm installs with the yum installer
    
    Signed-off-by: Joe deKoning <joe_dekoning@ca.ibm.com>

--- ansible/playbooks/scripts/AIX_filesystem_config.sh
+++ ansible/playbooks/scripts/AIX_filesystem_config.sh
@@ -23,6 +23,7 @@ fi
 #
 Total_Disk_Size=`getconf DISK_SIZE /dev/hdisk0`    								          # Determine overall size of disk
 Total_Disk_in_GB=`expr $Total_Disk_Size / 1024`    								          # Convert to GB
+Maximum_Processes=`lsattr -E -l sys0 | grep maxuproc | awk '{print $2}'`
 #
 if [[ $Total_Disk_in_GB -lt "40" ]]; then									                  # Disk is too small for script
 	echo "Error: Disk is too small (less than 40GB), manually configuration is required. Exiting..."
@@ -35,6 +36,7 @@ cur_usr=`df -g /usr | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 cur_var=`df -g /var | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 cur_opt=`df -g /opt | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 cur_tmp=`df -g /tmp | tail -1 | awk '{print $2}' | sed 's/\..*//'`
+cur_home=`df -g /home | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 #
 # Display Debug Information
 echo "******Debug Info: filesystem size******" 
@@ -45,6 +47,8 @@ echo "cur_usr:" $cur_usr
 echo "cur_var:" $cur_var
 echo "cur_opt:" $cur_opt
 echo "cur_tmp:" $cur_tmp
+echo "cur_home:" $cur_home
+echo "maxuproc:" $Maximum_Processes
 echo "**************************************"
 df -g
 echo "**************************************"
@@ -58,6 +62,15 @@ chlv -x 2048 $(df -g /usr | tail -1 | awk '{print $1}' | sed 's/\/dev\///')
 chlv -x 2048 $(df -g /var | tail -1 | awk '{print $1}' | sed 's/\/dev\///')
 chlv -x 2048 $(df -g /opt | tail -1 | awk '{print $1}' | sed 's/\/dev\///')
 chlv -x 2048 $(df -g /tmp | tail -1 | awk '{print $1}' | sed 's/\/dev\///')
+chlv -x 2048 $(df -g /home | tail -1 | awk '{print $1}' | sed 's/\/dev\///')
+#
+#############################################################
+# Ensure Maximum Processes for parallel jobs is high enough #
+#############################################################
+#
+if [[ $Maximum_Processes -le "512" ]]; then
+	chdev -l sys0 -a maxuproc=512
+fi
 #
 ##############################
 # Configure Filesystem Sizes #
@@ -102,7 +115,7 @@ if [[ $Total_Disk_in_GB -le "75" ]]; then
 	########
 	if [[ $cur_opt -lt "10" ]]; then
 		add_opt=`expr 10 - $cur_opt`
-		chfs -a size=+$add_usr"G" /opt
+		chfs -a size=+$add_opt"G" /opt
 	fi
 	if [[ $cur_opt -gt "10" ]]; then
 		chfs -a size=10G /opt
@@ -110,9 +123,19 @@ if [[ $Total_Disk_in_GB -le "75" ]]; then
 	########
 	# /tmp #
 	########
+	if [[ $cur_tmp -lt "30" ]]; then
+		add_tmp=`expr 30 - $cur_tmp`
+		chfs -a size=+$add_tmp"G" /tmp
+	fi
+	if [[ $cur_tmp -gt "30" ]]; then
+		chfs -a size=30G /tmp
+	fi
+	#########
+	# /home #
+	#########
 	unallocated_disk=`lsvg rootvg  | grep "FREE" | sed 's/.*FREE PPs//' | awk '{print $3}' | sed 's/(//'`
 	unall_disk_in_GB=`expr $unallocated_disk / 1024`
-	chfs -a size=+$unall_disk_in_GB"G" /tmp
+	chfs -a size=+$unall_disk_in_GB"G" /home
 fi 
 #
 #############################################################
@@ -162,15 +185,25 @@ if [[ $Total_Disk_in_GB -gt "75" ]] && [[ $Total_Disk_in_GB -le "125" ]]; then
 	########
 	# /tmp #
 	########
+        if [[ $cur_tmp -lt "30" ]]; then
+                add_tmp=`expr 30 - $cur_tmp`
+                chfs -a size=+$add_tmp"G" /tmp
+        fi
+        if [[ $cur_tmp -gt "30" ]]; then
+                chfs -a size=30G /tmp
+        fi
+        #########
+        # /home #
+        #########
 	unallocated_disk=`lsvg rootvg  | grep "FREE" | sed 's/.*FREE PPs//' | awk '{print $3}' | sed 's/(//'`
 	unall_disk_in_GB=`expr $unallocated_disk / 1024`
-	chfs -a size=+$unall_disk_in_GB"G" /tmp
+	chfs -a size=+$unall_disk_in_GB"G" /home
 fi
 #
-#################################
-# If disk is greater than 125GB #
-#################################
-if [[ $Total_Disk_in_GB -gt "125" ]]; then 
+##############################################################
+# If disk is less than or equal to 199GB but more than 125GB #
+##############################################################
+if [[ $Total_Disk_in_GB -gt "125" ]] && [[ $Total_Disk_in_GB -le "199" ]]; then  
 	##########
 	# root / #
 	##########
@@ -214,18 +247,51 @@ if [[ $Total_Disk_in_GB -gt "125" ]]; then
 	########
 	# /tmp #
 	########
+        if [[ $cur_tmp -lt "30" ]]; then
+                add_tmp=`expr 30 - $cur_tmp`
+                chfs -a size=+$add_tmp"G" /tmp
+        fi
+        if [[ $cur_tmp -gt "30" ]]; then
+                chfs -a size=30G /tmp
+        fi
+        #########
+        # /home #
+        #########
 	unallocated_disk=`lsvg rootvg  | grep "FREE" | sed 's/.*FREE PPs//' | awk '{print $3}' | sed 's/(//'`
 	unall_disk_in_GB=`expr $unallocated_disk / 1024`
 	if [[ $unall_disk_in_GB -gt "100" ]]; then
-		# Limit tmp to 100, leave the rest unallocated
-		# If tmp is already larger than 100 leave it as is
-		if [[ $cur_tmp -lt "100" ]]; then
-			add_tmp=`expr 100 - $cur_tmp`
-			chfs -a size=+add_tmp"G" /tmp
+		# Limit home to 100, leave the rest unallocated
+		# If home is already larger than 100 leave it as is
+		if [[ $cur_home -lt "100" ]]; then
+			add_home=`expr 100 - $cur_home`
+			chfs -a size=+$add_home"G" /home
 		fi
 	else
-		chfs -a size=+$unall_disk_in_GB"G" /tmp
+		chfs -a size=+$unall_disk_in_GB"G" /home
 	fi
+fi
+#
+#################################
+# If disk is greater than 200GB #
+#################################
+if [[ $Total_Disk_in_GB -ge "200" ]]; then 
+	chfs -a size=20G /
+	chfs -a size=15G /usr
+	chfs -a size=10G /var
+	chfs -a size=15G /opt
+	chfs -a size=30G /tmp
+        # Limit home to 100, leave the rest unallocated
+        # If home is already larger than 100 leave it as is
+        unallocated_disk=`lsvg rootvg  | grep "FREE" | sed 's/.*FREE PPs//' | awk '{print $3}' | sed 's/(//'`
+        unall_disk_in_GB=`expr $unallocated_disk / 1024`
+        if [[ $unall_disk_in_GB -gt "100" ]]; then
+                if [[ $cur_home -lt "100" ]]; then
+                        add_home=`expr 100 - $cur_home`
+                        chfs -a size=+$add_home"G" /home
+                fi
+        else
+                chfs -a size=+$unall_disk_in_GB"G" /home
+        fi
 fi
 #
 ##################################
@@ -237,6 +303,8 @@ new_usr=`df -g /usr | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 new_var=`df -g /var | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 new_opt=`df -g /opt | tail -1 | awk '{print $2}' | sed 's/\..*//'`
 new_tmp=`df -g /tmp | tail -1 | awk '{print $2}' | sed 's/\..*//'`
+new_home=`df -g /home | tail -1 | awk '{print $2}' | sed 's/\..*//'`
+new_Maximum_Processes=`lsattr -E -l sys0 | grep maxuproc | awk '{print $2}'`
 #
 # Display Debug Information
 echo "******Debug Info: filesystem size******" 
@@ -245,6 +313,8 @@ echo "new_usr:" $new_usr
 echo "new_var:" $new_var
 echo "new_opt:" $new_opt
 echo "new_tmp:" $new_tmp
+echo "new_home:" $new_home
+echo "maxuproc:" $new_Maximum_Processes
 echo "**************************************"
 df -g
 echo "**************************************"
