46a4002a56f46121fb0775c872ee5883d5addc14
George Adams <georgeadams1995@gmail.com>
2019-08-01 16:19:39 +0100

    add ppc64le and aarch64 test machines (#872)

--- ansible/plugins/inventory/adoptopenjdk_yaml.py
+++ ansible/plugins/inventory/adoptopenjdk_yaml.py
@@ -47,7 +47,7 @@ valid = {
   # providers - validated for consistency
   'provider': ('azure', 'joyent', 'marist', 'osuosl', 'scaleway',
         'macstadium', 'macincloud', 'softlayer', 'packet', 'linaro',
-        'digitalocean', 'ibm', 'godaddy')
+        'digitalocean', 'ibm', 'godaddy', 'openlab')
 }
 
 # customisation options per host:
