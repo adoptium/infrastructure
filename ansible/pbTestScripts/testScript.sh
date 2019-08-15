dadc1a4b122b287a3c9046eee51b5a4cb2b47c92
William Parker <52783864+Willsparker@users.noreply.github.com>
2019-08-15 12:34:44 +0100

    Added checks in PB test script to allow testing of branches (#896)
    * Added checks to allow for branches
    
    * Coding standards fix

--- ansible/pbTestScripts/testScript.sh
+++ ansible/pbTestScripts/testScript.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -eu
+set -u
+
+branchName='NULL'
+folderName=''
+gitURL=''
 
 # Takes all arguments from the script
 processArgs()
@@ -13,21 +17,34 @@ processArgs()
 
 setupFiles()
 {
-	cd ~
+	cd $HOME
 	mkdir -p adoptopenjdkPBTests || true
 	cd adoptopenjdkPBTests
 	mkdir -p logFiles || true
 }
 
-# Takes in git URL as arg 1, foldername as arg 2
+# Takes in git URL as arg 1, foldername as arg 2, branchName as arg 3
 setupGit()
 {
-	cd ~/adoptopenjdkPBTests
-	if [ ! -d "$2" ]; then		#if folder doesn't exist
-   		git clone $1
-	else				#if it does, ensure up to date
-		cd "$2"
-    		git pull $1
+	cd $HOME/adoptopenjdkPBTests
+	if [ "$branchName" == "NULL" ]; then
+		echo "Detected as the master branch"
+		if [ ! -d "$folderName-master" ]; then
+   			git clone $gitURL
+			mv $folderName $folderName-master
+		else
+			cd "$folderName-master"
+    			git pull $gitURL
+		fi
+	else
+		echo "Branch detected"
+		if [ ! -d "$folderName-$branchName" ]; then
+  			git clone -b $branchName --single-branch $gitURL
+			mv $folderName "$folderName-$branchName"
+		else
+			cd "$folderName-$branchName"
+			git pull origin $branchName
+		fi
 	fi
 }
 
@@ -37,15 +54,20 @@ testBuild()
 	vagrant ssh -c "cd /vagrant/pbTestScripts && ./buildJDK.sh"
 }
 
-# Takes the OS as arg 1, foldername as arg 2
+# Takes the OS as arg 1
 startVMPlaybook()
 {
-	cd ~/adoptopenjdkPBTests/$2/ansible
-	#Alias the correct vagrant file
-	ln -sf Vagrantfile.$1 Vagrantfile
+	local OS=$1
+	if [ "$branchName" == "NULL" ]; then
+		cd $HOME/adoptopenjdkPBTests/$folderName-master/ansible
+		$branchName="master"  #Incorrect!
+	else
+		cd $HOME/adoptopenjdkPBTests/$folderName-$branchName/ansible
+	fi
+	ln -sf Vagrantfile.$OS Vagrantfile
 	vagrant up
 	# Remotely moves to the correct directory in the VM and builds the playbook. Then logs the VM's output to a file, in a separate directory
-	vagrant ssh -c "cd /vagrant/playbooks/AdoptOpenJDK_Unix_Playbook && sudo ansible-playbook --skip-tags "adoptopenjdk,jenkins" main.yml" 2>&1 | tee ~/adoptopenjdkPBTests/logFiles/$2.$1.log
+	vagrant ssh -c "cd /vagrant/playbooks/AdoptOpenJDK_Unix_Playbook && sudo ansible-playbook --skip-tags "adoptopenjdk,jenkins" main.yml" 2>&1 | tee ~/adoptopenjdkPBTests/logFiles/$folderName.$branchName.$OS.log
 	testBuild
 	vagrant halt
 }
@@ -56,14 +78,14 @@ destroyVM()
 	vagrant destroy -f
 }
 
-# Takes in OS as arg 1
+# Takes in OS as arg 1, branchName as arg 2
 searchLogFiles()
 {
-	cd ~/adoptopenjdkPBTests/logFiles
-	if grep -q 'failed=[1-9]' *$1.log
+	cd $HOME/adoptopenjdkPBTests/logFiles
+	if grep -q 'failed=[1-9]' *$2.$1.log
 	then
 		printf "\n$1 Failed\n"
-	elif grep -q '\[ERROR\]' *$1.log
+	elif grep -q '\[ERROR\]' *$2.$1.log
 	then
 		printf "\n$1 playbook was stopped\n"
 	else
@@ -71,21 +93,41 @@ searchLogFiles()
 	fi
 }
 
+# Takes in the URL passed to the script, and extracts the foldername, branch name and builds the gitURL to be used later on.
+splitURL()
+{
+	local urlSplit
+	urlSplit='/' read -r -a array <<< "$1"
+	if [ ${array[@]: -2:1} == 'tree' ]
+	then
+		branchName=${array[@]: -1:1}
+		folderName=${array[@]: -3:1}
+		unset 'array[${#array[@]}-1]'
+		unset 'array[${#array[@]}-1]'
+		for i in "${array[@]}"
+		do
+			gitURL="$gitURL$i/"
+		done
+	else
+		folderName=${array[@]: -1:1}
+		gitURL=$1
+	fi
+}
 # var1 = GitURL, var2 = y/n for VM retention
-folderName=${1##*/}
 processArgs $*
+splitURL $1
 setupFiles
-setupGit $1 $folderName
+setupGit
 # For all tested OSs / Playbooks
-for OS in Ubuntu1804 Ubuntu1604 CentOS6
+for OS in Ubuntu1804 Ubuntu1604 CentOS6 CentOS7
 do
-	startVMPlaybook $OS $folderName
+	startVMPlaybook $OS
 	if [[ $2 = "n" ]]
 	then
 		destroyVM
 	fi
 done
-for OS in Ubuntu1804 Ubuntu1604 CentOS6
+for OS in Ubuntu1804 Ubuntu1604 CentOS6 CentOS7
 do
-	searchLogFiles $OS
+	searchLogFiles $OS $branchName
 done
