4ce9412236630ec87bd03e9a7305661e6c0a4604
William Parker <52783864+Willsparker@users.noreply.github.com>
2019-08-29 14:20:20 +0100

    Update to testScript.sh to allow for Parallelisation (#918)
    * Updated testScript.sh to allow playbook testing a single OS at a time to aid jenkins parallel execution

--- ansible/pbTestScripts/testScript.sh
+++ ansible/pbTestScripts/testScript.sh
@@ -1,33 +1,108 @@
 #!/bin/bash
 set -u
 
-branchName='NULL'
+branchName=''
 folderName=''
 gitURL=''
+vagrantOS=''
+retainVM=false
+testNativeBuild=false
 
-# Takes all arguments from the script
+# Takes all arguments from the script, and determines options
 processArgs()
 {
-	if [ $# -lt 2 ]; then
-		printf "\nScript takes 2 input arguments\n"
-		printf "Github URL, then y/n to retain VMs\n\n"
-		exit 1
+	while [[ $# -gt 0 ]] && [[ ."$1" = .-* ]] ; do
+		local opt="$1";
+		shift;
+		case "$opt" in
+			"--Vagrantfile" | "-v" )
+				vagrantOS="$1"; shift;;
+			"--all" | "-a" )
+				vagrantOS="all";;
+			"--build" | "-b" )
+				testNativeBuild=true;;
+			"--retainVM" | "-r" )
+				retainVM=true;;
+			"--URL" | "-u" )
+				gitURL="$1"; shift;;
+			"--help" | "-h" )
+				usage; exit 0;;
+			*) echo >&2 "Invalid option: ${opt}"; echo "This option was unrecognised."; usage; exit 1;;
+		esac
+	done
+}
+
+usage()
+{
+	echo
+	echo "Usage: ./testScript.sh			--vagrantfile | -v <OS_Version>		Specifies which OS the VM is
+					--all | -a 				Builds and tests playbook through every OS
+					--retainVM | -r				Option to retain the VM once building them
+					--build | -b				Option to enable testing a native build on the VM
+					--URL | -u <GitURL>			The URL of the git repository
+					--help | -h				Displays this help message"
+}
+
+defaultVars()
+{
+	#Sets WORKSPACE to home if WORKSPACE is empty or undefined. 
+	if [ ! -n "${WORKSPACE:-}" ]; then
+		echo "WORKSPACE not found, setting it as environment variable 'HOME'"
+		WORKSPACE=$HOME
 	fi
+	if [ "$gitURL" == "" ]; then
+		echo "No GitURL specified; Defaulting to adoptopenjdk/openjdk-infrastructure"
+		gitURL=https://github.com/adoptopenjdk/openjdk-infrastructure
+	fi
+	if [ "$vagrantOS" == "" ]; then
+		echo "No Vagrant OS specified; Defaulting to testing all of them"
+		vagrantOS="all"
+	fi
+}
+
+checkVagrantOS()
+{
+	case "$vagrantOS" in
+		"Ubuntu1604" | "U16" | "u16" )
+			vagrantOS="Ubuntu1604";;
+		"Ubuntu1804" | "U18" | "u18" )
+			vagrantOS="Ubuntu1804";;
+		"CentOS6" | "centos6" | "C6" | "c6" )
+			vagrantOS="CentOS6" ;;
+		"CentOS7" | "centos7" | "C7" | "c7" )
+			vagrantOS="CentOS7" ;;
+		"all" )
+			vagrantOS="Ubuntu1604 Ubuntu1804 CentOS6 CentOS7" ;;
+		*) echo "Not a currently supported OS" ; vagrantOSList; exit 1;
+	esac
+}
+
+vagrantOSList()
+{
+	echo
+	echo "Currently supported Vagrant OSs :
+		- Ubuntu1604
+		- Ubuntu1804
+		- CentOS6
+		- CentOS7"
+	echo
 }
 
 setupFiles()
 {
-	cd $HOME
-	mkdir -p adoptopenjdkPBTests || true
-	cd adoptopenjdkPBTests
-	mkdir -p logFiles || true
+	cd $WORKSPACE
+	if [ ! -d "adoptopenjdkPBTests" ]; then
+		mkdir adoptopenjdkPBTests
+	fi
+	if [ ! -d "adoptopenjdkPBTests/logFiles" ]; then
+		mkdir adoptopenjdkPBTests/logFiles
+	fi
 }
 
-# Takes in git URL as arg 1, foldername as arg 2, branchName as arg 3
 setupGit()
 {
-	cd $HOME/adoptopenjdkPBTests
-	if [ "$branchName" == "NULL" ]; then
+	cd $WORKSPACE/adoptopenjdkPBTests
+	if [ "$branchName" == "" ]; then
 		echo "Detected as the master branch"
 		if [ ! -d "$folderName-master" ]; then
    			git clone $gitURL
@@ -58,17 +133,19 @@ testBuild()
 startVMPlaybook()
 {
 	local OS=$1
-	if [ "$branchName" == "NULL" ]; then
-		cd $HOME/adoptopenjdkPBTests/$folderName-master/ansible
+	if [ "$branchName" == "" ]; then
+		cd $WORKSPACE/adoptopenjdkPBTests/$folderName-master/ansible
 		branchName="master"
 	else
-		cd $HOME/adoptopenjdkPBTests/$folderName-$branchName/ansible
+		cd $WORKSPACE/adoptopenjdkPBTests/$folderName-$branchName/ansible
 	fi
 	ln -sf Vagrantfile.$OS Vagrantfile
 	vagrant up
 	# Remotely moves to the correct directory in the VM and builds the playbook. Then logs the VM's output to a file, in a separate directory
-	vagrant ssh -c "cd /vagrant/playbooks/AdoptOpenJDK_Unix_Playbook && sudo ansible-playbook --skip-tags "adoptopenjdk,jenkins" main.yml" 2>&1 | tee ~/adoptopenjdkPBTests/logFiles/$folderName.$branchName.$OS.log
-	testBuild
+	vagrant ssh -c "cd /vagrant/playbooks/AdoptOpenJDK_Unix_Playbook && sudo ansible-playbook --skip-tags adoptopenjdk,jenkins main.yml" 2>&1 | tee ~/adoptopenjdkPBTests/logFiles/$folderName.$branchName.$OS.log
+	if [[ "$testNativeBuild" = true ]]; then
+		testBuild
+	fi
 	vagrant halt
 }
 
@@ -81,7 +158,7 @@ destroyVM()
 # Takes in OS as arg 1, branchName as arg 2
 searchLogFiles()
 {
-	cd $HOME/adoptopenjdkPBTests/logFiles
+	cd $WORKSPACE/adoptopenjdkPBTests/logFiles
 	if grep -q 'failed=[1-9]' *$2.$1.log
 	then
 		printf "\n$1 Failed\n"
@@ -93,13 +170,14 @@ searchLogFiles()
 	fi
 }
 
-# Takes in the URL passed to the script, and extracts the foldername, branch name and builds the gitURL to be used later on.
+# Takes in the URL passed to the script, and extracts the folder name, branch name and builds the gitURL to be used later on.
 splitURL()
 {
 	#IFS stands for Internal Field Seperator and determines the delimiter for splitting.
-	IFS='/' read -r -a array <<< "$1"
+	IFS='/' read -r -a array <<< "$gitURL"
 	if [ ${array[@]: -2:1} == 'tree' ]
 	then
+		gitURL=""
 		branchName=${array[@]: -1:1}
 		folderName=${array[@]: -3:1}
 		unset 'array[${#array[@]}-1]'
@@ -110,24 +188,25 @@ splitURL()
 		done
 	else
 		folderName=${array[@]: -1:1}
-		gitURL=$1
 	fi
 }
 # var1 = GitURL, var2 = y/n for VM retention
 processArgs $*
-splitURL $1
+defaultVars
+splitURL
+checkVagrantOS
 setupFiles
 setupGit
-# For all tested OSs / Playbooks
-for OS in Ubuntu1804 Ubuntu1604 CentOS6 CentOS7
+echo "Testing on the following OSs: $vagrantOS"
+for OS in $vagrantOS
 do
 	startVMPlaybook $OS
-	if [[ $2 = "n" ]]
+	if [[ "$retainVM" = false ]]
 	then
 		destroyVM
 	fi
 done
-for OS in Ubuntu1804 Ubuntu1604 CentOS6 CentOS7
+for OS in $vagrantOS
 do
 	searchLogFiles $OS $branchName
 done
