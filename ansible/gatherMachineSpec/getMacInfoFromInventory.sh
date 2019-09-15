0617a48ea6355b815d380eae0b2f185016ff59b7
mani <sadhak001@gmail.com>
2019-09-15 15:55:21 +0100

    Added checks to record unable to access via ssh and also failing to gather info after ssh access

--- ansible/gatherMachineSpec/getMacInfoFromInventory.sh
+++ ansible/gatherMachineSpec/getMacInfoFromInventory.sh
@@ -6,14 +6,46 @@ set -o pipefail
 
 nmapscan() {
   host_ip=$1
-  CAN_SSH="$(nmap ${host_ip} --host-timeout 10 --max-retries 5 | grep -i ssh || true)"
+  SCAN_RESULTS="$(nmap ${host_ip} --host-timeout 10 --max-retries 5 | grep -i ssh || true)"
   sleep 1
-  ### On some machines, checking twice reveals the ssh port
-  if [ -z "${CAN_SSH}" ]; then
-    CAN_SSH="$(nmap ${host_ip} --host-timeout 10 --max-retries 5 | grep -i ssh || true)"
+  ### On some machines, checking twice reveals the ssh port after a small gap
+  if [ -z "${SCAN_RESULTS}" ]; then
+    SCAN_RESULTS="$(nmap ${host_ip} --host-timeout 10 --max-retries 5 | grep -i ssh || true)"
   fi
-  echo ${CAN_SSH}
+  echo ${SCAN_RESULTS}
 } 
+
+recordMachineLoginStatus() {
+  exitCode=$1
+  host_ip=$2
+  servername=$3
+  if [[ ${exitCode} -eq 0 ]]; then
+    if [[ -z "$(grep ${host_ip} successfully-accessed-machines.txt || true)" ]]; then
+       echo "${servername},${host_ip}" >> successfully-accessed-machines.txt
+       echo "Succeeded in accessing machine via ssh"
+    fi
+  else
+    if [[ -z "$(grep ${host_ip} failed-to-access-machines.txt || true)" ]]; then
+       echo "${servername},${host_ip}" >> failed-to-access-machines.txt
+       echo "Failed to access machine via ssh"
+    fi
+  fi
+}
+
+checkIfSSHIsPossible() {
+  host_ip=$1
+  servername=$2
+  echo "Checking to see if we can ssh on to ip ${host_ip}"
+  CAN_SSH=$(nmapscan ${host_ip})
+  echo "nmap scan results: '${CAN_SSH}'"
+
+  if [[ -z "${CAN_SSH}" ]]; then
+    echo "Cannot ssh onto this machine, skipping gathering info, moving to next machine..."
+    if [[ -z "$(grep ${host_ip} ssh-not-available-machines.txt || true)" ]]; then
+       echo "${servername},${host_ip}" >> ssh-not-available-machines.txt
+    fi
+  fi
+}
 
 SSH_KEY="${1:-"-i ${HOME}/.ssh/id_rsa-linux"}"
 
@@ -27,8 +59,10 @@ if [[ ! -s "machines.txt" ]]; then
   sed -i machines.txt 's/ /,/g' machines.txt
 fi
 
-totalCount=$(cat machines.txt | wc -l | tr -d ' ')
+totalCount=$(cat machines.txt | wc | awk '{print $2}')
 count=0
+
+CAN_SSH=""
 
 machines=( $( cat machines.txt ) )
 for machine in "${machines[@]}"
@@ -42,27 +76,25 @@ do
   echo ""
   echo "[${count}/${totalCount}] Scanning ${servername} with ip '${host_ip}' and username '${user}'"
 
-  echo "Checking to see if we can ssh on to ip ${host_ip}"
-  CAN_SSH=$(nmapscan ${host_ip})
-  echo "nmap scan results: '${CAN_SSH}'"
-
+  checkIfSSHIsPossible ${host_ip} ${servername}
   if [[ -z "${CAN_SSH}" ]]; then
-    echo "Cannot ssh onto this machine, skipping gathering info, moving to next machine..."
     continue
   fi
 
-set -x
+  exitCode=0
   if [[ ! -z "$(echo ${servername} | grep -i win)" ]]; then
        echo "Windows box"
-       ssh -T "${user}@${host_ip}" ${SSH_KEY} < windows.sh || true 
+       ssh -t ${SSH_KEY} "${user}@${host_ip}" < windows.sh || exitCode=$? && true
   elif [[ ! -z "$(echo ${servername} | grep -i macos)" ]]; then
         echo "MacOS box"
-        ssh -T "${user}@${host_ip}" ${SSH_KEY} < macos.sh || true 
+        ssh -t ${SSH_KEY} "${user}@${host_ip}" < macos.sh || exitCode=$? && true
   else
         echo "Unix/Linux box"
-        ssh -T "${user}@${host_ip}" ${SSH_KEY} < linux.sh || true 
+        ssh -t ${SSH_KEY} "${user}@${host_ip}" < linux.sh || exitCode=$? && true
   fi
-set +x  
+
+  recordMachineLoginStatus ${exitCode} ${host_ip} ${servername}
+
   echo "-----------------------------------------------------------------------------"
 done
 
