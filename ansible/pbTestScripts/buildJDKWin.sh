16dc49fd44a734dba8c497821ef475f7144b1895
Will Parker <william.parker1@ibm.com>
2019-09-20 08:29:14 +0100

    Changed variant to hotspot; changed JDK_BOOT_DIR to JDK8_BOOT_DIR

--- ansible/pbTestScripts/buildJDKWin.sh
+++ ansible/pbTestScripts/buildJDKWin.sh
@@ -3,9 +3,9 @@ set -eu
 export TARGET_OS=linux
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
-export VARIANT=openj9
+export VARIANT=hotspot
 export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
-export JDK_BOOT_DIR=/usr/lib/jvm/java-8-openjdk-amd64
+export JDK8_BOOT_DIR=/usr/lib/jvm/java-8-openjdk-amd64
 echo $PATH
 export PATH=/usr/bin:$PATH 
 /cygdrive/c/openjdk-build/build-farm/make-adopt-build-farm.sh
