126a5ae651932681fac583b96529a5cc1ae07f11
Will Parker <william.parker1@ibm.com>
2019-09-20 15:16:56 +0100

    Changed OS to windows

--- ansible/pbTestScripts/buildJDKWin.sh
+++ ansible/pbTestScripts/buildJDKWin.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -eu
-export TARGET_OS=linux
+export TARGET_OS=windows
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=hotspot
 export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
 export JDK8_BOOT_DIR=/usr/lib/jvm/java-8-openjdk-amd64
-echo $PATH
 export PATH=/usr/bin:$PATH 
 /cygdrive/c/openjdk-build/build-farm/make-adopt-build-farm.sh
