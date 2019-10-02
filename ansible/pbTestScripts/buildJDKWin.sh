6749d642af3ff0ff26f9e7b40602d6ff473b572c
Will Parker <william.parker1@ibm.com>
2019-10-02 11:23:06 +0100

    Changed environment variables for Windows building a JDK

--- ansible/pbTestScripts/buildJDKWin.sh
+++ ansible/pbTestScripts/buildJDKWin.sh
@@ -4,7 +4,7 @@ export TARGET_OS=windows
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=hotspot
-export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
-export JDK8_BOOT_DIR=/usr/lib/jvm/java-8-openjdk-amd64
+export JDK7_BOOT_DIR=C:/openjdk/jdk7
+export JDK11_BOOT_DIR=C:/openjdk/jdk-11
 export PATH=/usr/bin:$PATH 
 /cygdrive/c/openjdk-build/build-farm/make-adopt-build-farm.sh
