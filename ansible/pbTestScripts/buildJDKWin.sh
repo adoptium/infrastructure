5e5d1809ea1336ff394b5d4767e3fa1f19a75708
Will Parker <william.parker1@ibm.com>
2019-10-14 12:18:16 +0100

    pbTest: Changed path for JDK variables

--- ansible/pbTestScripts/buildJDKWin.sh
+++ ansible/pbTestScripts/buildJDKWin.sh
@@ -4,7 +4,7 @@ export TARGET_OS=windows
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=hotspot
-export JDK7_BOOT_DIR=C:/openjdk/jdk7
-export JDK11_BOOT_DIR=C:/openjdk/jdk-11
+export JDK7_BOOT_DIR=cygdrive/c/openjdk/jdk7
+export JDK11_BOOT_DIR=cygdrive/c/openjdk/jdk-11
 export PATH=/usr/bin:$PATH 
 /cygdrive/c/openjdk-build/build-farm/make-adopt-build-farm.sh
