49931f4d99d04a05160375c2e67f95ab11d441aa
Will Parker <william.parker1@ibm.com>
2019-10-21 11:54:58 +0100

    PbTest: Add JAVA_HOME var for buildJDK script

--- ansible/pbTestScripts/buildJDK.sh
+++ ansible/pbTestScripts/buildJDK.sh
@@ -9,7 +9,8 @@ export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=openj9
 export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
-export JDK_BOOT_DIR=/usr/lib/jvm/java-8-openjdk-amd64
+export JDK_BOOT_DIR=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 cd $WORKSPACE/openjdk-build
 build-farm/make-adopt-build-farm.sh
 
