21f70689802ea4ed19d4b9e47bf1885f219c5ad2
Will Parker <william.parker1@ibm.com>
2019-11-18 09:34:57 +0000

    pbTests: Updated vmDestroy.sh

--- ansible/pbTestScripts/vmDestroy.sh
+++ ansible/pbTestScripts/vmDestroy.sh
@@ -1,50 +1,58 @@
 #!/bin/bash
 set -eu
 
+osToDestroy=''
+
 # Takes in all arguments
 processArgs()
 {
-	if [ ! -n "${WORKSPACE:-}" ]; then
-		echo "WORKSPACE not found, setting it as environment variable 'HOME'"
-		WORKSPACE=$HOME
-	fi
 	if [ $# -lt 1 ]; then
 		echo "Script takes 1 input argument: "
-		echo "The project whose VMs are to be destroyed"
+		echo "The OS of the VMs you want to destroy."
 		exit 1
 	fi
-	
 }
 
-# Takes project name as arg 1, and OS as arg 2
-destroyVM()
-{
-	cd $WORKSPACE/adoptopenjdkPBTests/$1/ansible
-	ln -sf Vagrantfile.$2 Vagrantfile	# Correct Vagrantfile alias
-	vagrant destroy -f			# Force destroy without question
+checkOS() {
+	local OS=$1
+        case "$OS" in
+                "Ubuntu1604" | "U16" | "u16" )
+			osToDestroy="U16";;
+                "Ubuntu1804" | "U18" | "u18" )
+                        osToDestroy="U18";;
+                "CentOS6" | "centos6" | "C6" | "c6" )
+                        osToDestroy="C6" ;;
+                "CentOS7" | "centos7" | "C7" | "c7" )
+                        osToDestroy="C7" ;;
+                "Windows2012" | "Win2012" | "W12" | "w12" )
+                        osToDestroy="W2012";;
+                "all" )
+                        osToDestroy="U16 U18 C6 C7 W2012" ;;
+                *) echo "Not a currently supported OS" ; listOS; exit 1;
+        esac
+}
+
+listOS() {
 	echo
-	echo "Destroyed $2 Machine"	
+	echo "Currently supported OSs:
+		- Ubuntu1604
+		- Ubuntu1804
+		- CentOS6
+		- CentOS7
+		- Win2012"
+	echo
 }
 
-# Takes the project name as arg1
-checkFolder()
-{
-	cd $WORKSPACE/adoptopenjdkPBTests
-	if [ -d "$1" ]; then
-		echo "$1 found!"
-		return 0
-	else
-		echo "$1 not found"
-		exit 1
-	fi
+destroyVMs() {
+	local OS=$1
+	vagrant global-status | grep "adoptopenjdk$OS" | awk '{ print $1 }' | xargs -I {} vagrant destroy -f {}
+	echo "Destroyed all $OS Vagrant VMs"
 }
 
-# Script takes project name as arg 1
 processArgs $*
-if checkFolder $1; then	
- 	# For all currently supported OSs
-	for OS in Ubuntu1804 Ubuntu1604 CentOS6 CentOS7 Win2012
-	do
-		destroyVM $1 $OS
-	done
-fi
+checkOS $1
+for OS in $osToDestroy
+do
+	destroyVMs $OS
+done
+
