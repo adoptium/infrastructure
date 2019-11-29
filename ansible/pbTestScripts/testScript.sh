31535079af93beb78dce9299f52097ce8561e5b0
William Parker <52783864+Willsparker@users.noreply.github.com>
2019-11-29 16:39:33 +0000

    pbTest: Remove $gitURL and use rm -f (#1022)
    * pbTest: Remove gitURL from git pull; use rm -f
    
    * Forced fixes following review

--- ansible/pbTestScripts/testScript.sh
+++ ansible/pbTestScripts/testScript.sh
@@ -132,7 +132,7 @@ setupGit()
 			mv $folderName $folderName-master
 		else
 			cd "$folderName-master"
-    			git pull $gitURL
+    			git pull 
 		fi
 	else
 		echo "Branch detected"
@@ -160,7 +160,7 @@ startVMPlaybook()
 
 	ln -sf Vagrantfile.$OS Vagrantfile
 	# Copy the machine's ssh key for the VMs to use, after removing prior files
-	[[ -f id_rsa.pub ]] && rm id_rsa.pub id_rsa
+	rm -f id_rsa.pub id_rsa
 	ssh-keygen -q -f $PWD/id_rsa -t rsa -N ''
 	vagrant up
 	# Generate hosts.unx file for Ansible to use, remove prior hosts.unx if there
