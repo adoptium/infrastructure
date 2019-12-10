8f02fedecf085089860407cfe1d2d7bbfbb89b6d
Will Parker <william.parker1@ibm.com>
2019-12-10 09:12:14 +0000

    pbTests: Check and set GCC to version 7; find correct Java_8 for CentOS And Ubuntu

--- ansible/pbTestScripts/buildJDK.sh
+++ ansible/pbTestScripts/buildJDK.sh
@@ -6,13 +6,18 @@ if [ -z "${WORKSPACE:-}" ]; then
 		WORKSPACE=$HOME
 fi
 [[ ! -d "$WORKSPACE/openjdk-build" ]] && git clone https://github.com/adoptopenjdk/openjdk-build $WORKSPACE/openjdk-build
+
+# If detected GCC version is <7, set it to 7 
+export gccVer=$(gcc --version | grep "gcc" | awk '{ print $4 }' | cut -d. -f1)
+[[ ${gccVer} -lt 7 ]] && ln -sf /usr/bin/gcc-7 /usr/bin/gcc && ln -sf /usr/bin/g++-7 /usr/bin/g++
+
 export TARGET_OS=linux
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=openj9
 export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
-export JDK_BOOT_DIR=/usr/lib/jvm/java-1.8.0-openjdk-amd64
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+# Differences in openJDK8 name between Ubuntu and CentOS
+export JAVA_HOME=$(find /usr/lib/jvm/ -name java-1.8.0-openjdk-\*)
 cd $WORKSPACE/openjdk-build
 build-farm/make-adopt-build-farm.sh
 
