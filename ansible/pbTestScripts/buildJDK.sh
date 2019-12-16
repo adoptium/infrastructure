26d8ffcb771521a7a5c816f6608085fb1c91d36b
William Parker <52783864+Willsparker@users.noreply.github.com>
2019-12-16 15:37:46 +0000

    pbTests: Find correct JDK versions for different OSs (#1034)
    Check and set correct Java_8 for CentOS And Ubuntu

--- ansible/pbTestScripts/buildJDK.sh
+++ ansible/pbTestScripts/buildJDK.sh
@@ -6,13 +6,19 @@ if [ -z "${WORKSPACE:-}" ]; then
 		WORKSPACE=$HOME
 fi
 [[ ! -d "$WORKSPACE/openjdk-build" ]] && git clone https://github.com/adoptopenjdk/openjdk-build $WORKSPACE/openjdk-build
+
 export TARGET_OS=linux
 export ARCHITECTURE=x64
 export JAVA_TO_BUILD=jdk8u
 export VARIANT=openj9
-export JDK7_BOOT_DIR=/usr/lib/jvm/java-1.7.0
-export JDK_BOOT_DIR=/usr/lib/jvm/java-1.8.0-openjdk-amd64
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
+# Differences in openJDK7 name between OSs. Search for CentOS one
+export JDK7_BOOT_DIR=$(find /usr/lib/jvm/ -name java-1.7.0-openjdk.x86_64)
+# If the CentOS JDK7 can't be found, search for the Ubuntu one
+[[ -z "$JDK7_BOOT_DIR" ]] && export JDK7_BOOT_DIR=$(find /usr/lib/jvm/ -name java-1.7.0-openjdk-\*)
+
+# Differences in openJDK8 name between Ubuntu and CentOS
+export JAVA_HOME=$(find /usr/lib/jvm/ -name java-1.8.0-openjdk-\*)
 cd $WORKSPACE/openjdk-build
 build-farm/make-adopt-build-farm.sh
 
