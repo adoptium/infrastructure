d9b00a2a933b0c6f24da995aa5ecbe46442f1dce
William Parker <52783864+Willsparker@users.noreply.github.com>
2019-12-19 11:46:15 +0000

    pbTests: Altered Windows Scripts to build and test (#1053)

--- ansible/pbTestScripts/testJDKWin.sh
+++ ansible/pbTestScripts/testJDKWin.sh
@@ -29,7 +29,7 @@ fi
 cd openjdk-tests
 
 ./get.sh -t $HOME/testLocation/openjdk-tests -p x64_windows
-cd TestConfig
+cd TKG
 make -f run_configure.mk AUTO_DETECT=true
 export BUILD_LIST=MachineInfo
 make compile
