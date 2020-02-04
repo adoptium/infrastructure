2b3cd1e0863493229b2feb176bef745056b5a4f8
Will Parker <william.parker1@ibm.com>
2020-02-04 15:41:02 +0000

    Ensure script stops if playbook fails

--- ansible/pbTestScripts/qemu_test_script.sh
+++ ansible/pbTestScripts/qemu_test_script.sh
@@ -169,7 +169,11 @@ runPlaybook() {
 	[[ ! -d "$workFolder/openjdk-infrastructure"  ]] && git clone https://github.com/adoptopenjdk/openjdk-infrastructure "$workFolder"/openjdk-infrastructure
 	cd "$workFolder"/openjdk-infrastructure/ansible || exit 1;
 	ansible-playbook -i "localhost:$PORTNO," --private-key "$workFolder"/id_rsa -u linux -b --skip-tags adoptopenjdk,jenkins${skipFullSetup} playbooks/AdoptOpenJDK_Unix_Playbook/main.yml 2>&1 | tee "$workFolder"/logFiles/"$ARCHITECTURE".log
-
+	if grep -q 'failed[1-9]\|unreachable=[1-9]' "$workFolder"/logFiles/"$ARCHITECTURE".log then
+		echo "Playbook failed"
+		destroyVM
+		exit 1;
+	fi
 	if [[ "$buildJDK" == true ]]; then
 		ssh linux@localhost -p "$PORTNO" -i "$workFolder"/id_rsa "git clone https://github.com/adoptopenjdk/openjdk-infrastructure \$HOME/openjdk-infrastructure && \$HOME/openjdk-infrastructure/ansible/pbTestScripts/buildJDK.sh"
 		if [[ "$testJDK" == true ]]; then
