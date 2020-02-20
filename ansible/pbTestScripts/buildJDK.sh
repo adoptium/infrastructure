a51346c4ef4999117df1fbdc3f85b1e4b0d48ed4
Will Parker <william.parker1@ibm.com>
2020-02-20 12:09:57 +0000

    Add various options to buildJDK.sh

--- ansible/pbTestScripts/buildJDK.sh
+++ ansible/pbTestScripts/buildJDK.sh
@@ -1,6 +1,63 @@
 #!/bin/bash
 set -eu
 
+processArgs() {
+	while [[ $# -gt 0 ]] && [[ ."$1" = .-* ]] ; do
+		local opt="$1";
+		shift;
+		case "$opt" in
+			"--version" | "-v" )
+				JAVA_TO_BUILD="$1"; shift;;
+			"--URL" | "-u" )
+				GIT_URL="$1"; shift;;
+			"--openj9" | "-j9" )
+				VARIANT=openj9;;
+			"--clean-workspace" | "-c" )
+				CLEAN_WORKSPACE=true;;
+			"--help" | "-h" )
+				usage; exit 0;;
+			*) echo >&2 "Invalid option: ${opt}"; echo "This option was unrecognised."; usage; exit 1;;
+		esac
+	done
+
+	checkJDKVersion $JAVA_TO_BUILD
+	if [ -z "${WORKSPACE:-}" ]; then
+        	echo "WORKSPACE not found, setting it as environment variable 'HOME'"
+        	WORKSPACE=$HOME
+	fi
+	if [ $CLEAN_WORKSPACE == true ]; then
+		echo "Cleaning workspace"
+		rm -rf $WORKSPACE/openjdk-build
+	fi
+}
+
+usage() {
+	echo "Usage: ./buildJDK.sh <options>
+	
+	Options:
+		--version | -v		Specify the JDK version to build
+		--URL | -u		Specify the github URL to clone openjdk-build from
+		--openj9 | -j9		Builds openJ9, instead of hotspot
+		--clean-workspace | -c 	Removes old openjdk-build folder before cloning
+		--help | -h		Shows this message
+		
+	If not specified, JDK8-HS will be built with the standard openjdk-build repo"
+	echo
+	showJDKVersions
+}
+
+showJDKVersions() {
+	echo "Currently supported JDK versions: 
+		- JDK8
+		- JDK9
+		- JDK10
+		- JDK11
+		- JDK12
+		- JDK13
+		- JDK14"
+	echo
+}
+
 checkJDKVersion() {
         local jdk=$1
         case "$jdk" in
@@ -19,30 +76,43 @@ checkJDKVersion() {
                 "jdk14u" | "jdk14" | "14" | "14u" )
                         JAVA_TO_BUILD="jdk14u";;
                 *)
-                        echo "Not a valid JDK Version" ; exit 1;;
+                        echo "Not a valid JDK Version" ; showJDKVersions; exit 1;;
         esac
 }
 
-# Argument parsing
+cloneRepo() {
+	local branch=""
+	IFS='/' read -r -a urlArray <<< "$GIT_URL"
+	if [ -d $WORKSPACE/openjdk-build ]; then
+		echo "Found existing openjdk-build folder"
+		cd $WORKSPACE/openjdk-build && git pull
+	elif [ ${urlArray[@]: -2:1} == 'tree' ]; then
+		GIT_URL=""
+		echo "Branch detected"
+		branch=${urlArray[@]: -1:1}
+		unset 'urlArray[${#urlArray[@]}-1]'
+		unset 'urlArray[${#urlArray[@]}-1]'
+		for urlPart in "${urlArray[@]}"
+		do
+			GIT_URL="$GIT_URL$urlPart/"
+		done
+		git clone -b $branch $GIT_URL $WORKSPACE/openjdk-build
+	else
+		echo "No branch detected"
+		git clone $GIT_URL $WORKSPACE/openjdk-build
+	fi
+}
+
+# Default values
 export JAVA_TO_BUILD=jdk8u
-if [[ $# == 0 ]]; then
-        echo "No arguments input, defaulting to JDK8"
-elif [[ $# == 1 ]]; then
-	checkJDKVersion $1
-else
-        echo "Too many arguments"
-        exit 1;
-fi
-
 export PATH=/usr/local/bin/:$PATH
-if [ -z "${WORKSPACE:-}" ]; then
-	echo "WORKSPACE not found, setting it as environment variable 'HOME'"
-	WORKSPACE=$HOME
-fi
-
 export TARGET_OS=linux
-export VARIANT=openj9
+export VARIANT=hotspot
 export ARCHITECTURE=x64
+GIT_URL="https://github.com/adoptopenjdk/openjdk-build"
+CLEAN_WORKSPACE=false
+
+processArgs $*
 
 # Differences in openJDK7 name between OSs. Search for CentOS one
 export JDK7_BOOT_DIR=$(find /usr/lib/jvm/ -name java-1.7.0-openjdk.x86_64)
@@ -76,13 +146,10 @@ echo "DEBUG:
         VARIANT=$VARIANT
         JDK7_BOOT_DIR=$JDK7_BOOT_DIR
         JAVA_HOME=$JAVA_HOME
-        WORKSPACE=$WORKSPACE"
+        WORKSPACE=$WORKSPACE
+	GIT_URL=$GIT_URL"
 
-if [[ ! -d "$WORKSPACE/openjdk-build" && "$TARGET_OS" == "FreeBSD" ]]; then
-  git clone -b freebsd https://github.com/gdams/openjdk-build $WORKSPACE/openjdk-build
-elif [[ ! -d "$WORKSPACE/openjdk-build" ]]; then
-  git clone https://github.com/adoptopenjdk/openjdk-build $WORKSPACE/openjdk-build
-fi
+cloneRepo 
 
 cd $WORKSPACE/openjdk-build
 build-farm/make-adopt-build-farm.sh
