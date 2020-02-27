bd838b6cdf6f0d98d20ed72801d2ed5f04b2487e
Will Parker <william.parker1@ibm.com>
2020-02-27 13:56:32 +0000

    pbTests: Alter location of FreeBSD built JDK

--- ansible/pbTestScripts/testJDK.sh
+++ ansible/pbTestScripts/testJDK.sh
@@ -3,9 +3,13 @@
 export MAKE_COMMAND="make"
 if [[ $(uname) == "FreeBSD" ]]; then
 	export MAKE_COMMAND="gmake"
+	cp -r $HOME/openjdk-build/workspace/build/src/build/*/jdk $HOME
+	export TEST_JDK_HOME=$HOME/jdk
+else
+	mv $HOME/openjdk-build/workspace/build/src/build/*/images/jdk8* $HOME
+	export TEST_JDK_HOME=$(find $HOME -maxdepth 1 -type d -name "*jdk8u*"|grep -v ".*jre.*")
 fi
-mv $HOME/openjdk-build/workspace/build/src/build/*/images/jdk8* $HOME
-export TEST_JDK_HOME=$(find $HOME -maxdepth 1 -type d -name "*jdk8u*"|grep -v ".*jre.*")
+
 mkdir -p $HOME/testLocation
 [ ! -d $HOME/testLocation/openjdk-tests ] && git clone https://github.com/adoptopenjdk/openjdk-tests $HOME/testLocation/openjdk-tests
 $HOME/testLocation/openjdk-tests/get.sh -t $HOME/testLocation/openjdk-tests
