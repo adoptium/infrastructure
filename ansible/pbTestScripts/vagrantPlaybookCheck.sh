1ddea155e4201275e0985a71bf7a03091b16a94e
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2020-10-14 14:50:51 +0100

    Update ansible/pbTestScripts/vagrantPlaybookCheck.sh
    Co-authored-by: Will Parker <52783864+Willsparker@users.noreply.github.com>

--- ansible/pbTestScripts/vagrantPlaybookCheck.sh
+++ ansible/pbTestScripts/vagrantPlaybookCheck.sh
@@ -233,7 +233,7 @@ startVMPlaybook()
 	echo "[127.0.0.1]:${vagrantPORT}" >> playbooks/AdoptOpenJDK_Unix_Playbook/hosts.unx
 	# Remove IP from known_hosts if already found
 	# ssh-keygen -R will fail if the known_hosts file does not exist
-        [ ! -r $HOME/.ssh/known_hosts ] && touch $HOME/.ssh/known_hosts && chmod 644 $HOME/.ssh/known_hosts
+	[ ! -r $HOME/.ssh/known_hosts ] && touch $HOME/.ssh/known_hosts && chmod 644 $HOME/.ssh/known_hosts
 	ssh-keygen -R $(cat playbooks/AdoptOpenJDK_Unix_Playbook/hosts.unx)
 	
 	sed -i -e "s/.*hosts:.*/- hosts: all/g" playbooks/AdoptOpenJDK_Unix_Playbook/main.yml
