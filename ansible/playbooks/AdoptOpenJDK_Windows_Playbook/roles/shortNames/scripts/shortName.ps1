49d3355bdbfce097c5b02c93dbd4f0e66ea610e0
George Adams <georgeadams1995@gmail.com>
2022-06-07 11:48:20 +0100

    add shortname fix

--- ansible/playbooks/AdoptOpenJDK_Windows_Playbook/roles/shortNames/scripts/shortName.ps1
+++ ansible/playbooks/AdoptOpenJDK_Windows_Playbook/roles/shortNames/scripts/shortName.ps1
@@ -4,6 +4,13 @@ $dirName=$Args[0]
 $shortName=$Args[1]
 
 $string=(cmd /c dir /x "C:\Program Files (x86)" | grep "$dirName")
+
+# Skip the directory if it doesn't exist
+If ($string -eq $null){
+	echo "Directory not found - skipping"
+	exit
+}
+
 $result=($string.split(" ")[17])
 
 If ($result -eq ""){
