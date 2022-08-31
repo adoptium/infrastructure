58b99839ce3b8d5168699f6f775e41573f2d72db
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2022-08-31 15:40:43 +0100

    Remove duplicate path setting.

--- ansible/pbTestScripts/testJDK.sh
+++ ansible/pbTestScripts/testJDK.sh
@@ -14,16 +14,13 @@ echo DEBUG: TEST_JDK_HOME = $TEST_JDK_HOME
 
 # Special case for Solaris. See: https://github.com/adoptium/infrastructure/pull/2405#issuecomment-999498345
 if [[ "$(uname)" == "SunOS" ]]; then
-	export PATH="/opt/csw/bin:/usr/local/bin:${PATH}"
+	export PATH="/usr/local/bin:/opt/csw/bin:${PATH}"
 fi
 
 mkdir -p $HOME/testLocation
 [ ! -d $HOME/testLocation/aqa-tests ] && git clone https://github.com/adoptium/aqa-tests.git $HOME/testLocation/aqa-tests
 # cd to aqa-tests as required by https://github.com/adoptium/aqa-tests/issues/2691#issue-932959102
 cd $HOME/testLocation/aqa-tests
-if [[ "$(uname)" == "SunOS" ]]; then
-	export PATH=/usr/local/bin:$PATH
-fi
 $HOME/testLocation/aqa-tests/get.sh
 cd $HOME/testLocation/aqa-tests/TKG || exit 1
 
@@ -31,7 +28,6 @@ cd $HOME/testLocation/aqa-tests/TKG || exit 1
 # See: https://adoptium.slack.com/archives/C53GHCXL4/p1641311568115100?thread_ts=1641296204.114900&cid=C53GHCXL4
 if [[ "$(uname)" == "SunOS" ]]; then
 	export BUILD_LIST=system
-	export PATH=/usr/local/bin:$PATH
 	$MAKE_COMMAND compile
 	$MAKE_COMMAND _MachineInfo
 else
