bb70a0400668ae17669fcc3fe03d9e1978d5e5c1
julian55455 <julianatuhwera55@gmail.com>
2022-10-23 02:02:02 +0300

    Refactoring

--- ansible/playbooks/nagios/roles/Nagios_Server/scripts/nagios_host_group_add.sh
+++ ansible/playbooks/nagios/roles/Nagios_Server/scripts/nagios_host_group_add.sh
@@ -3,14 +3,14 @@
 echo "enter the hostgroup name"
 read hostgroup_name
 
-echo "enter the hostgroup elias"
-read hostgroup_elias
+echo "enter the hostgroup alias"
+read hostgroup_alias
 
 echo "enter enter the hostgroup members"
 read hostgroup_members
 
 echo "define hostgroup{" >> /usr/local/nagios/etc/objects/hostgroups.cfg
 echo "        hostgroup_name  $hostgroup_name" >> /usr/local/nagios/etc/objects/hostgroups.cfg
-echo "        alias           $hostgroup_elias" >> /usr/local/nagios/etc/objects/hostgroups.cfg
+echo "        alias           $hostgroup_alias" >> /usr/local/nagios/etc/objects/hostgroups.cfg
 echo "        members         $hostgroup_members" >> /usr/local/nagios/etc/objects/hostgroups.cfg
 echo "        }" >> /usr/local/nagios/etc/objects/hostgroups.cfg
