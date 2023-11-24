fb4bd0dada60199b24b312308e6bb3d203d128de
Scott Fryer <scfryer@redhat.com>
2023-11-24 08:32:43 +0000

    Update Vagrant clean script

--- ansible/pbTestScripts/vmDestroy.sh
+++ ansible/pbTestScripts/vmDestroy.sh
@@ -47,6 +47,8 @@ checkOS() {
                         osToDestroy="U20";;
                 "Ubuntu2104" | "U21" | "u21" )
                         osToDestroy="U21";;
+								"Ubuntu2204" | "U22" | "u22" )
+				                        osToDestroy="U22";;
                 "CentOS6" | "centos6" | "C6" | "c6" )
                         osToDestroy="C6" ;;
                 "CentOS7" | "centos7" | "C7" | "c7" )
@@ -66,7 +68,7 @@ checkOS() {
   	"Windows2022" | "Win2022" | "W22" | "w22" )
 	                       osToDestroy="W2022";;
 	              "all" )
-                        osToDestroy="U16 U18 U20 U21 C6 C7 C8 D8 D10 FBSD12 Sol10 W2012" ;;
+                        osToDestroy="U16 U18 U20 U21 U22 6 C7 C8 D8 D10 FBSD12 Sol10 W2012 W2022" ;;
 		"")
 			echo "No OS detected. Did you miss the '-o' option?" ; usage; exit 1;;
 		*) echo "$OS is not a currently supported OS" ; listOS; exit 1;
@@ -80,6 +82,7 @@ listOS() {
 		- Ubuntu1804
 		- Ubuntu2004
 		- Ubuntu2104
+		- Ubuntu2204
 		- CentOS6
 		- CentOS7
 		- CentOS8
