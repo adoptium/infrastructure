4c909968b6696f00db0d01f171058e4eaf9f2506
Haroon Khel <hkhel@redhat.com>
2023-12-20 14:21:35 +0000

    add fontconfig to common packages

--- ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/updatepackages.sh
+++ ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/updatepackages.sh
@@ -4,7 +4,7 @@ set -u
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 containerIds=$(docker ps -q)
 
-commonPackages="gnupg fakeroot"
+commonPackages="gnupg fakeroot fontconfig"
 fedoraPackages="procps-ng hostname shared-mime-info"
 debianPackages=""
 alpinePackages=""
