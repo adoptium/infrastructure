998ce4a3b534d9aea09e1a0220f8d05376d9a9b6
George Adams <georgeadams1995@gmail.com>
2024-04-23 15:44:33 +0200

    unixPB: add new rise paid for scaleway RISC-V hosts

--- ansible/plugins/inventory/adoptopenjdk_yaml.py
+++ ansible/plugins/inventory/adoptopenjdk_yaml.py
@@ -49,7 +49,7 @@ valid = {
   'provider': ('alibaba', 'azure', 'marist', 'osuosl',
         'macstadium', 'macincloud', 'ibmcloud', 'siteox',
         'equinix', 'linaro','digitalocean', 'ibm', 'godaddy',
-        'aws', 'inspira', 'equinix_esxi', 'nine', 'scaleway', 'skytap',
+        'aws', 'inspira', 'equinix_esxi', 'nine', 'rise', 'skytap',
         'hetzner')
 }
 
