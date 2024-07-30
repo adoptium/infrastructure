5da2c0397fe62c13ea03daff34bf9e1ecf70c75f
Haroon Khel <hkhel@redhat.com>
2024-07-30 12:29:01 +0100

    Add condition in vpc script to set python version

--- ansible/pbTestScripts/vagrantPlaybookCheck.sh
+++ ansible/pbTestScripts/vagrantPlaybookCheck.sh
@@ -267,6 +267,12 @@ startVMPlaybook()
 
 	rm -f playbooks/AdoptOpenJDK_Unix_Playbook/hosts.unx
 	echo "[127.0.0.1]:${vagrantPORT}" >> playbooks/AdoptOpenJDK_Unix_Playbook/hosts.unx
+
+	# For OSs that require an earlier version of python to be compatible with the version of Ansible on the host
+	if [[ "$OS" == "Fedora40" ]]; then
+		echo "[127.0.0.1]:${vagrantPORT} ansible_python_interpreter=/usr/bin/python3.8" > playbooks/AdoptOpenJDK_Unix_Playbook/hosts.unx
+	fi
+	
 	# Remove IP from known_hosts if already found
 	# ssh-keygen -R will fail if the known_hosts file does not exist
 	[ ! -r $HOME/.ssh/known_hosts ] && touch $HOME/.ssh/known_hosts && chmod 644 $HOME/.ssh/known_hosts
