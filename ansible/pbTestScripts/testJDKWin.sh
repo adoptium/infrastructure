758841031c8923719431679c6dcd63ebc0372fe0
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2024-12-03 12:10:52 +0000

    Vagrant: Improve Test Coverage & Align Tests In VPC process. (#3832)
    * VPC: Improve VPC JDK Test Process
    
    * VPC: Improve windows tests
    
    * Vagrant: Increase Fedora 40 Disksize
    
    * Vagrant: Add Additional Tests To VPC Testing.

--- ansible/pbTestScripts/testJDKWin.sh
+++ ansible/pbTestScripts/testJDKWin.sh
@@ -15,6 +15,9 @@ rm -rf /cygdrive/c/tmp/*-test-image
 export TEST_JDK_HOME=`ls -d c:/tmp/jdk*|grep -v "static"|grep -v "debug"|grep -v "jre"|grep -v "test-image"`
 echo TEST_JDK_HOME=$TEST_JDK_HOME
 
+## Run The Same Tests As Test JDK for Linux
+## Run The Smoke Tests To Ensure The JDK Build OK
+
 cd /cygdrive/c/tmp
 if [ ! -d "testLocation" ];
 then
@@ -22,15 +25,27 @@ then
 	mkdir testLocation
 fi
 cd testLocation
-if [ ! -d "openjdk-tests" ];
-then
-	echo "Git cloning openjdk-tests"
-	git clone https://github.com/adoptopenjdk/openjdk-tests
-fi
-cd openjdk-tests
+git clone https://github.com/adoptium/aqa-tests.git
+pwd
+ls -tr
+cd aqa-tests
+./get.sh --vendor_repos https://github.com/adoptium/temurin-build --vendor_branches master --vendor_dirs /test/functional
+pwd
+ls -ltr
+cd TKG || exit 1
 
-./get.sh -T $HOME/testLocation/openjdk-tests -p x64_windows
-cd TKG
-export BUILD_LIST=system
+## Run The Smoke Tests To Ensure The JDK Build OK
+export BUILD_LIST=functional/buildAndPackage
 make compile
-make _extended.system 
+make _extended.functional
+
+# Run a few subsets of OpenJDK Tests as a shakedown of the built JDK.
+export BUILD_LIST=openjdk
+make compile
+make _hotspot_sanity_0
+make _jdk_math_0
+
+# Run Some Additional Tests To Test The Playbooks Have Run Properly
+export BUILD_LIST=functional
+make _MBCS_Tests_pref_ja_windows_0
+make _MBCS_Tests_formatter_ja_windows_0
