c4a51d8a1856d54a6529b3acee175c4d5d136640
Haroon Khel <hkhel@redhat.com>
2025-07-31 15:53:05 +0100

    Use sed instead of awk to support double digits cpu

--- ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/deploy_container/scripts/dockerCPU.sh
+++ ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/deploy_container/scripts/dockerCPU.sh
@@ -3,20 +3,27 @@
 # Usage - Script takes one argument, the number of cpus for the docker container
 # ie bash dockerCPU.sh 4
 # Argument cannot be 0 or greater than the number of CPUs on the machine
-# If /var/log/docker_cpu does not exist, run: echo 0 > /var/log/docker_cpu
+# If /etc/adoptium_docker_cpu does not exist, run: echo 0 > /etc/adoptium_docker_cpu
 
 set -eu
 
 cpu_limit=$1
+
+if [ ! -f /etc/adoptium_docker_cpu ]; then
+    echo "ERROR: /etc/adoptium_docker_cpu does not exist. Exiting script"
+    exit 1
+fi
+
 start_cpu=$(tail -n 1 /etc/adoptium_docker_cpu)
-max_cpus=$(lscpu | grep "CPU(s):" | head -n 1 | awk '{split($0,a," "); print a[2]}')
-cpus_to_use=""
+max_cpus=$(lscpu | grep "CPU(s):" | head -n 1 | sed 's/.* //')
 
 if [ $cpu_limit -gt $max_cpus ] || [ $cpu_limit == "0" ]
 then
     echo "ERROR: The container cannot have more cpus than there are cpus on the host or be 0"
     exit 1
 fi
+
+cpus_to_use=""
 
 for i in $(seq 0 $(($cpu_limit - 1)));
 do
@@ -25,4 +32,4 @@ done
 
 # Output: comma separated list of cpus to use, and next cpu to use
 echo $cpus_to_use | awk '{sub(",","")}1'
-echo $(((${cpus_to_use: -1} + 1) % $max_cpus))
+echo $((($(echo $cpus_to_use | sed "s/.*,//") + 1) % $max_cpus))
